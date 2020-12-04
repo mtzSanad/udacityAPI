04e7a9c17f05a90e87dc9d6240a83a82f2d7a256
Moataz Sanad <mtz.sanad@gmail.com>
2020-12-04 09:10:59 +0200

    end of endpoints

--- flasker/__init__.py
+++ flasker/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 def create_app(test_config=None):
     app = Flask(__name__)
+    # CORS(app)
+    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+    # CORS Headers 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
 
     @app.route('/')
+    @cross_origin()
     def hello():
         return jsonify({'message':'Hello mizo'})
 
